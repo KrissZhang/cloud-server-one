b0de0e9680aa6a6f0fe44e71a48b43bb664ad850
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2021-09-03 16:07:40 +0800

    提交

--- cloud-server-one-server/src/main/java/com/self/cloudserver/mapper/RoleMapper.java
+++ cloud-server-one-server/src/main/java/com/self/cloudserver/mapper/RoleMapper.java
@@ -2,7 +2,11 @@ package com.self.cloudserver.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.self.cloudserver.entity.Role;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface RoleMapper extends BaseMapper<Role> {
+
+    Role queryRole(Role role);
 
 }
